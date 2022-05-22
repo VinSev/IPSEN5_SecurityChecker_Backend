504f21042f66b2de23f11ae8442d7ecd0a2697dd
Bruno <bruno.pleysier@gmail.com>
2022-05-22 23:01:14 +0200

    entrypoint pdf service

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/service/PdfService.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/service/PdfService.java
@@ -1,4 +1,23 @@
 package nl.hsleiden.IPSEN5_SecurityChecker_Backend.service;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+
+import java.io.IOException;
+
 public class PdfService {
+
+
+
+
+    public void makePdf() throws IOException {
+        PDDocument document = new PDDocument();
+        PDPage page = document.getPage(1);
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        contentStream.beginText();
+        contentStream.newLineAtOffset(25, 700);
+        contentStream.endText();
+    }
 }
