a805a97448431d5a751f7e9481b09e473a94bbfb
Bruno <bruno.pleysier@gmail.com>
2022-05-24 20:13:27 +0200

    pdf tablemaker update

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/controller/PdfController.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/controller/PdfController.java
@@ -1,8 +1,11 @@
 package nl.hsleiden.IPSEN5_SecurityChecker_Backend.controller;
 
 
+import nl.hsleiden.IPSEN5_SecurityChecker_Backend.model.ScanCategory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/pdf")
@@ -15,7 +18,12 @@ public class PdfController {
     @PostMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
-    public void getPdfInfo(@RequestBody String name,String website){
+    public void getPdfInfo(@RequestBody ArrayList<ScanCategory> scans,
+                           @RequestBody String name,
+                           @RequestBody String website){
         System.out.println(name + website);
+//        scans.forEach(scan ->{
+//            System.out.println(scan);
+//        });
     }
 }
