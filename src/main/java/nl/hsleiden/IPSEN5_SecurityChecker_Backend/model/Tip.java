13ddc7fee144f038a93d8b711f55320b9f0adf0c
VinSe <vincentmcmaps@gmail.com>
2022-05-24 18:17:56 +0200

    auto gen id

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/Tip.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/Tip.java
@@ -1,14 +1,13 @@
 package nl.hsleiden.IPSEN5_SecurityChecker_Backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tips")
 public class Tip {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @NotBlank
     private String text;
