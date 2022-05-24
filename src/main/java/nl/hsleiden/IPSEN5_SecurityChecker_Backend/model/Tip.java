e6984d03050985fe41a4a73db2f999399a324ea5
VinSe <vincentmcmaps@gmail.com>
2022-05-24 18:24:37 +0200

    bug fixes

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/Tip.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/Tip.java
@@ -7,8 +7,24 @@ import javax.validation.constraints.NotBlank;
 @Table(name = "tips")
 public class Tip {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     @NotBlank
     private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
