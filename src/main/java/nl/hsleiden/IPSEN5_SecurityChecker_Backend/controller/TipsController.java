81cc01428263e5a5c45eab14c2087b3dced7e590
VinSe <vincentmcmaps@gmail.com>
2022-05-24 18:09:06 +0200

    added tip controller and service

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/controller/TipsController.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/controller/TipsController.java
@@ -1,38 +1,52 @@
 package nl.hsleiden.IPSEN5_SecurityChecker_Backend.controller;
 
+import nl.hsleiden.IPSEN5_SecurityChecker_Backend.model.Tip;
+import nl.hsleiden.IPSEN5_SecurityChecker_Backend.service.TipsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/tips")
 @CrossOrigin
 public class TipsController {
+    @Autowired
+    private TipsService tipsService;
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public void get() {
+    public List<Tip> getAll() {
+        return this.tipsService.getAll();
+    }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Tip get(@PathVariable("id") Integer id) {
+        return this.tipsService.get(id);
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
-    public void post() {
-
+    public Tip post(@RequestBody Tip tip) {
+        return this.tipsService.create(tip);
     }
 
     @PutMapping()
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public void put() {
-
+    public void put(@RequestBody Tip tip) {
+        this.tipsService.update(tip);
     }
 
     @DeleteMapping()
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public void delete() {
-
+    public void delete(@RequestBody Tip tip) {
+        this.tipsService.delete(tip);
     }
 }
--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/service/TipsService.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/service/TipsService.java
@@ -1,8 +1,33 @@
 package nl.hsleiden.IPSEN5_SecurityChecker_Backend.service;
 
+import nl.hsleiden.IPSEN5_SecurityChecker_Backend.model.Tip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TipsService {
+    @Autowired
+    private TipsService tipsService;
 
+    public List<Tip> getAll() {
+        return this.tipsService.getAll();
+    }
+
+    public Tip get(Integer id) {
+        return this.tipsService.get(id);
+    }
+
+    public Tip create(Tip tip) {
+        return this.tipsService.create(tip);
+    }
+
+    public void update(Tip tip) {
+        this.tipsService.update(tip);
+    }
+
+    public void delete(Tip tip) {
+        this.tipsService.delete(tip);
+    }
 }
