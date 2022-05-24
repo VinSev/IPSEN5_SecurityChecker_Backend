b20971412e79b21954b41724ab322859123bd47a
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-05-24 17:03:22 +0200

    Inloggen

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/UserRequest.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/UserRequest.java
@@ -4,6 +4,11 @@ public class UserRequest {
     private String email;
     private String password;
 
+    public UserRequest(String email, String password) {
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+
     public String getEmail() {
         return email;
     }
