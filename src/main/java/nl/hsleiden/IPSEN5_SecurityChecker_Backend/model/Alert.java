4d36f364cbf8cfc57ce30e5693a09fcc36f50b35
Bruno <bruno.pleysier@gmail.com>
2022-06-10 14:24:00 +0200

    makes stuff in json

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/Alert.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/model/Alert.java
@@ -1,4 +1,14 @@
 package nl.hsleiden.IPSEN5_SecurityChecker_Backend.model;
 
 public class Alert {
+    private String score;
+    private String titel;
+    private boolean geslaagd;
+
+    public Alert(String score, String titel, boolean geslaagd) {
+        this.score = score;
+        this.titel = titel;
+        this.geslaagd = geslaagd;
+    }
+
 }
--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/utility/ActiveScan.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/utility/ActiveScan.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 public class ActiveScan {
@@ -23,7 +24,7 @@ public class ActiveScan {
     private static final String ZAP_ADDRESS = "localhost";
     private static final String TARGET = "https://webshop-vacations.herokuapp.com/";
 
-    public static void attackScan(){
+    public static void attackScan() {
 
         ClientApi api = new ClientApi(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY);
 
@@ -59,20 +60,20 @@ public class ActiveScan {
         }
     }
 
-    public static void spiderSearch () {
+    public static void spiderSearch() {
 
         ClientApi api = new ClientApi(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY);
 
         try {
 
-            // Start spidering the target
+
             System.out.println("Ajax Spider target : " + TARGET);
             ApiResponse resp = api.ajaxSpider.scan(TARGET, null, null, null);
             String status;
 
             long startTime = System.currentTimeMillis();
-            long timeout = TimeUnit.MINUTES.toMillis(2); // Two minutes in milli seconds
-            // Loop until the ajax spider has finished or the timeout has exceeded
+            long timeout = TimeUnit.MINUTES.toMillis(2);
+
             while (true) {
                 Thread.sleep(2000);
                 status = (((ApiResponseElement) api.ajaxSpider.status()).getValue());
@@ -82,23 +83,17 @@ public class ActiveScan {
                 }
             }
             System.out.println("Ajax Spider completed");
-            // Perform additional operations with the Ajax Spider results
-            List<ApiResponse> ajaxSpiderResponse = ((ApiResponseList) api.ajaxSpider.results("0", "10")).getItems();
 
-            // TODO: Start scanning(passive/active scan) the application to find vulnerabilities
+
             System.out.println("Active Scanning target : " + TARGET);
-            ApiResponse resp2 = api.ascan.scan(TARGET, "True", "False", null, null, null);
-            int start = 0;
-            int count = 5000;
-            int alertCount = 0;
             String scanid;
             int progress;
-            List<String> blackListPlugins = Arrays.asList("1000", "1025");
 
 
-            // The scan now returns a scan id to support concurrent scanning
+
+
             scanid = ((ApiResponseElement) resp).getValue();
-            // Poll the status until it completes
+
             while (true) {
                 Thread.sleep(5000);
                 progress =
@@ -113,46 +108,39 @@ public class ActiveScan {
             System.out.println("Active Scan complete");
 
             System.out.println("Alerts:");
-            System.out.println(new String(api.core.xmlreport(), StandardCharsets.UTF_8));
-            byte[] reporto = api.core.jsonreport();
-            System.out.println("YELLLING AT THE SUNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN");
 
-            ObjectMapper mapper = new ObjectMapper();
-            Map<String,Object> map = mapper.readValue(reporto, Map.class);
+            byte[] reporto = api.core.jsonreport();
+
 
             JSONObject jsonnn = new JSONObject(new String(reporto));
 
 
             System.out.println(jsonnn);
-            System.out.println(jsonnn.getJSONArray("site"));
+            JSONArray jsonarray = jsonnn.getJSONArray("site");
+            Object jsonobj = jsonarray.get(0);
+            System.out.println(jsonarray);
 
-            JSONArray sites = new JSONArray(jsonnn.getJSONArray("site"));
+            ObjectMapper oMapper = new ObjectMapper();
 
-
-            System.out.println(sites);
-
-
-            int size = sites.length();
-
-            for (int i = 0; i < size; i++) {
-                JSONObject another_json_object = sites.getJSONObject(i);
-                System.out.println(another_json_object);
-                System.out.println(another_json_object.get("alerts"));
-
-                PrintWriter out = new PrintWriter(new FileWriter("G:\\uniwerk\\programmeren\\java.json"));
-                out.write(another_json_object.toString());
-            }
-
-
-
-
-            FileWriter xmlFile = new FileWriter("report.txt");
-            xmlFile.write(new String(api.core.xmlreport(), StandardCharsets.UTF_8));
+//            for (int j = 0; j < jsonarray.length(); j++) {
+//                System.out.println("test");
+//                for (int k = 0; k < jsonarray.getJSONArray(j).length(); k++) {
+//                    String country = jsonarray.getJSONArray(j).getJSONObject(k).getString("riskdesc");
+//                    Integer count = jsonarray.getJSONArray(j).getJSONObject(k).getInt("name");
+//                    System.out.println(count);
+//                    cargo.put(country, count);
+//                }
+//            }
 
 
 
 
 
+
+//
+//            FileWriter xmlFile = new FileWriter("report.txt");
+//            xmlFile.write(new String(api.core.xmlreport(), StandardCharsets.UTF_8));
+//
 
 
         } catch (Exception e) {
