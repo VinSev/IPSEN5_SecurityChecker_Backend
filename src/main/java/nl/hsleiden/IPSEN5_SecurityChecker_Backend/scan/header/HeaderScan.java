2fa0ded844dd640efa9df07fe7240856a5330efd
VinSe <vincentmcmaps@gmail.com>
2022-06-21 14:28:11 +0200

    scan grade fix

--- src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/scan/header/HeaderScan.java
+++ src/main/java/nl/hsleiden/IPSEN5_SecurityChecker_Backend/scan/header/HeaderScan.java
@@ -28,6 +28,23 @@ public class HeaderScan extends AbstractScan {
             scanId = getScanId();
             TimeUnit.SECONDS.sleep(1);
         } while (!isFinished());
+
+        Map<String, Integer> grades = new HashMap<>();
+        grades.put("A+", 10);
+        grades.put("A", 9);
+        grades.put("A-", 9);
+        grades.put("B+", 8);
+        grades.put("B", 8);
+        grades.put("B-", 7);
+        grades.put("C+", 7);
+        grades.put("C", 6);
+        grades.put("C-", 5);
+        grades.put("D+", 4);
+        grades.put("D", 3);
+        grades.put("D-", 2);
+        grades.put("F", 1);
+        int grade = grades.get(super.getResult().getString("grade"));
+        super.setGrade(grade);
     }
 
     private void startScan() throws IOException, InterruptedException {
@@ -64,23 +81,6 @@ public class HeaderScan extends AbstractScan {
 
         HttpResponse<String> response = http.send(request, HttpResponse.BodyHandlers.ofString());
         setResult(new JSONObject(response.body()));
-
-        Map<String, Integer> grades = new HashMap<>();
-        grades.put("A+", 10);
-        grades.put("A", 9);
-        grades.put("A-", 9);
-        grades.put("B+", 8);
-        grades.put("B", 8);
-        grades.put("B-", 7);
-        grades.put("C+", 7);
-        grades.put("C", 6);
-        grades.put("C-", 5);
-        grades.put("D+", 4);
-        grades.put("D", 3);
-        grades.put("D-", 2);
-        grades.put("F", 1);
-        int grade = grades.get(super.getResult().getString("grade"));
-        super.setGrade(grade);
 
         return super.getResult().getInt("scan_id");
     }
